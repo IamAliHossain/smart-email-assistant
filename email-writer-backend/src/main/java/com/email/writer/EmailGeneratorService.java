da328e337713357feaf988e2644bc58ca66ee13c
alihossain.cse.pstu@gmail.com <alihossain.cse.pstu@gmail.com>
2025-10-05 23:51:39 +0600

    backend code is added

---
+++ email-writer-backend/src/main/java/com/email/writer/EmailGeneratorService.java
@@ -0,0 +1,86 @@
+package com.email.writer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+//@AllArgsConstructor
+public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String getGeminiApiKey;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String generateEmailReply(EmailRequest emailRequest){
+        // build prompt
+        String prompt = buildPrompt(emailRequest);
+
+        // craft request : Request crafting মানে হলো নিজের মতো করে request বানিয়ে বা modify করে পাঠানো,
+        // যাতে সিস্টেমের behavior test করা যায় বা vulnerability exploit করা যায়
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text", prompt)
+                        })
+                }
+        );
+
+        // Do request and get response
+        String response = webClient.post()
+                .uri(geminiApiUrl + getGeminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        // Extract Response and Return
+        return ExtractResponseContent(response);
+    }
+
+    private String ExtractResponseContent(String response) {
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        }catch (Exception e){
+            return "Error Processing request : " + e.getMessage();
+
+        }
+
+    }
+
+    // building the prompt
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line ");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+
+        return prompt.toString();
+
+    }
+}
